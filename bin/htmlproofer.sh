277489cd4de86c77ea31eb5ed73c2c97a6f95a80
David Crossley <david@indexdata.com>
2020-05-20 16:50:42 +1000

    Exclude more known

--- bin/htmlproofer.sh
+++ bin/htmlproofer.sh
@@ -9,9 +9,11 @@ IGNORE="/dev\.folio\.org/,/localhost:/"
 IGNORE+=",/folio-org\/jenkins-pipeline-libs/"
 IGNORE+=",/folio-org-priv\/folio-infrastructure/"
 IGNORE+=",/folio-testing-okapi\.aws/"
+IGNORE+=",/folio-testing-test.*\/settings/"
 
 # Ignore some known broken ones
 IGNORE+=",/#mod-vendors/"
+IGNORE+=",/api\/mod-codex-mock/"
 
 # Temporarily ignore github. Getting 429 error.
 IGNORE+=",/github\.com\/folio-org/"
