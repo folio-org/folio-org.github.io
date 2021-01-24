19ff7a767f1f21f946869aeccc38c97c22620db4
David Crossley <david@indexdata.com>
2021-01-25 10:27:19 +1100

    Show more api-related

--- .github/workflows/list-repos-api.sh
+++ .github/workflows/list-repos-api.sh
@@ -1,3 +1,13 @@
 INPUT_PN="${GITHUB_WORKSPACE}/_data/repos.json"
-echo "isSpringway:"
+echo "# ----------"
+echo "# isSpringway:"
 jq -r '.repos[] | if .isSpringway then .name else empty end' < ${INPUT_PN}
+echo "# ----------"
+echo "# hintOas:"
+jq -r '.repos[] | if .hintOas then .name else empty end' < ${INPUT_PN}
+echo "# ----------"
+echo "# isRmb:"
+jq -r '.repos[] | if .isRmb then .name else empty end' < ${INPUT_PN}
+echo "# ----------"
+echo "# hasRaml:"
+jq -r '.repos[] | if .ramlDirName then .name else empty end' < ${INPUT_PN}
