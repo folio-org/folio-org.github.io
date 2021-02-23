930e8753ee02ca348038b71baf968ec9765a5211
David Crossley <david@indexdata.com>
2021-02-23 16:18:32 +1100

    Install yarn dependencies

---
+++ .github/workflows/gather-schema-changes/deref-schema.js
@@ -0,0 +1,24 @@
+const RefParser = require("@apidevtools/json-schema-ref-parser");
+const path = require('path')
+const fs = require('fs')
+
+const inFn = process.argv[2];
+const inPath = path.resolve(__dirname, inFn)
+const outFn = process.argv[3];
+const outPath = path.resolve(__dirname, outFn)
+
+RefParser.dereference(inPath, (err, schema) => {
+  if (err) {
+    process.exitCode = 1
+    console.error(err);
+  }
+  else {
+    fs.writeFile(outPath, JSON.stringify(schema, null, 2), (err) => {
+      if (err) {
+        process.exitCode = 1
+        console.error(err);
+      }
+    })
+  }
+})
+
