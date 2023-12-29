3173b9861484e13b44b615d93bdc5e857f50ff84
David Crossley <david@indexdata.com>
2023-12-29 15:41:39 +1100

    Tidy deref-schema.js

--- .github/workflows/gather-schema-changes/deref-schema.js
+++ .github/workflows/gather-schema-changes/deref-schema.js
@@ -1,30 +1,26 @@
-const RefParser = require("@apidevtools/json-schema-ref-parser");
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+const RefParser = require('@apidevtools/json-schema-ref-parser');
 
-const inFn = process.argv[2];
-const inPath = path.resolve(__dirname, inFn);
-const outFn = process.argv[3];
-const outPath = path.resolve(__dirname, outFn);
+const inPath = path.resolve(__dirname, process.argv[2]);
+const outPath = path.resolve(__dirname, process.argv[3]);
 
 const parserOptions = {
-  "dereference": {
-    "circular": "ignore"
-  }
+  dereference: {
+    circular: 'ignore',
+  },
 };
 
 RefParser.dereference(inPath, parserOptions, (err, schema) => {
   if (err) {
     process.exitCode = 1;
     console.error(err);
-  }
-  else {
-    fs.writeFile(outPath, JSON.stringify(schema, null, 2), (err) => {
-      if (err) {
+  } else {
+    fs.writeFile(outPath, JSON.stringify(schema, null, 2), (err2) => {
+      if (err2) {
         process.exitCode = 1;
-        console.error(err);
+        console.error(err2);
       }
-    })
+    });
   }
 });
-
